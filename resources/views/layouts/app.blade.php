1c90018b519bb151b84fbf176668550080af0d93
praveen6379311990 <veradispraveen@gmail.com>
2025-05-08 20:03:05 +0530

    udemy 50% completed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,8 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    @yield('styles')
 </head>
 <body>
+    @if(session()->has('success'))
+    <div>{{session('success')}}</div>
+    @endif
     <h2>@yield('title')</h2>
     @yield('content')
 </body>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Requests\TaskRequest;
 use App\Models\Task;
+use Illuminate\Http\Request;
+// use Illuminate\Http\Client\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get("/", function () {
@@ -9,15 +12,32 @@ Route::get("/", function () {
 
 Route::view('/tasks/create','create');
 
+Route::post('/tasks',function(TaskRequest $request){
+    $task = Task::create($request->validated());
+    return redirect()->route('tasks.show',['task'=>$task->id])->with('success', 'Task created succesfully');
+})->name('tasks.store');
+
+Route::get('/tasks/{task}/edit',function(Task $task){
+    return view('edit',[
+        'task'=>$task
+    ]);
+})->name('tasks.edit');
+
+Route::put('/tasks/{task}',function(Task $task,TaskRequest $request){
+    $task->update($request->validated());
+    return redirect()->route('tasks.show',['task'=>$task->id])->with('success', 'Task updated succesfully');
+
+})->name('tasks.update');
+
 Route::get('/tasks', function () {
     return view('index',[
         'tasks'=>Task::latest()->where('completed',true)->get()
     ]);
 })->name('tasks.index');
 
-Route::get('/tasks/{id}',function($id){
+Route::get('/tasks/{task}',function(Task $task){
     return view('show',[
-        'task'=>Task::findOrFail($id)
+        'task'=>$task
     ]);
 })->name('tasks.show');
 
